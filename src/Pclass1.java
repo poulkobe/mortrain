3266903063fb4c95269eda044702c99c66f6e728
pavlogrebeniuk <poulkobr@gmail.com>
2017-08-18 16:04:18 +0300

    p init pavel test1 uuuu

--- src/Pclass1.java
+++ src/Pclass1.java
@@ -3,7 +3,7 @@ public class Pclass1 {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test1");
-        System
+        System.out.print("uuu");
     }
 }
 
